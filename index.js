e739f33db556d51aaf7b3fca56b2dead92355b80
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-06-19 20:14:09 +0500

    update controller to manage todo items

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var app = angular.module("angularJsTodo", []);
 
 app.controller("todoCtrl", function($scope) {
-  
+  $scope.todo = {
+    list: [],
+    tempItem: ''
+  };
+  $scope.addNewTodoItem = function() {
+    $scope.todo.list.push($scope.todo.tempItem);
+    $scope.todo.tempItem = ''
+    console.log($scope.todo.list);
+  } 
 });
