bdd2c01724b5e7ef65f6ac6cad6994ffeac95e99
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-06-20 20:21:38 +0500

    fixed linting

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var app = angular.module("angularJsTodo", []);
 
-app.controller("todoCtrl", function($scope) {
+app.controller("todoCtrl", function ($scope) {
   $scope.todo = {
     list: [],
-    tempItem: ''
+    tempItem: "",
   };
-  $scope.addNewTodoItem = function() {
+  $scope.addNewTodoItem = function () {
     $scope.todo.list.push($scope.todo.tempItem);
-    $scope.todo.tempItem = ''
-  } 
-  $scope.removeTodoItem = function(index) {
-    $scope.todo.list.splice(index,1);
-  }
+    $scope.todo.tempItem = "";
+  };
+  $scope.removeTodoItem = function (index) {
+    $scope.todo.list.splice(index, 1);
+  };
 });
